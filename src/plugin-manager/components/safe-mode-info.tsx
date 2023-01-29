43a1fd7430f8966208f2c6aba83dd1a51c95923c
MicroBlock <microblock@qq.com>
2023-01-29 10:34:47 +0800

    feat: reload button in safemode

--- src/plugin-manager/components/safe-mode-info.tsx
+++ src/plugin-manager/components/safe-mode-info.tsx
@@ -1,4 +1,5 @@
-import { getLoadError } from "../../loader";
+import { disableSafeMode, getLoadError } from "../../loader";
+import { Button } from "./button";
 
 export const SafeModeInfo: React.FC = () => {
 	const loadError = React.useMemo(getLoadError, []);
@@ -22,6 +23,16 @@ export const SafeModeInfo: React.FC = () => {
 						插件加载器已经收集了每次加载发生的错误，请确认加载失败的插件，并将发生错误的插件手动移除或修正。
 					</p>
 					<p>完成调整后，可以通过按下重载插件关闭安全模式并重新加载插件。</p>
+
+					<Button
+						onClick={async () => {
+							await disableSafeMode();
+							betterncm_native.app.restart();
+						}}
+					>
+						重启并重载插件
+					</Button>
+
 					{loadError.length === 0 ? (
 						<p>
 							没有找到加载错误记录，有可能是受到插件影响或插件管理器自身出错。
