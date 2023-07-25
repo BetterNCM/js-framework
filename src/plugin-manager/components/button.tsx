a711afd17cc5a1d5987bc856b0238ee5aa3a6a96
MicroBlock <microblock@qq.com>
2023-07-25 14:59:41 +0800

    fix for 2.10.x

--- src/plugin-manager/components/button.tsx
+++ src/plugin-manager/components/button.tsx
@@ -2,7 +2,8 @@ export const Button: React.FC<
 	React.PropsWithChildren<React.HTMLAttributes<HTMLAnchorElement>>
 > = (props) => {
 	const { children, className, ...other } = props;
-	const wrapperClass = [...((document.querySelector('.cmd-button'))!.classList as unknown as string[])].find(v => v.startsWith('ButtonWrapper_'))
+	const is300 = APP_CONF.appver.startsWith('3.')
+	const wrapperClass = is300 && [...((document.querySelector('.cmd-button'))!.classList as unknown as string[])].find(v => v.startsWith('ButtonWrapper_'))
 	return (
 		<a className={`${wrapperClass} u-ibtn5 u-ibtnsz8 cmd-button cmd-button-outlineSec cmd-button-size-m cmd-button-outline-sec button-item
 		 ${className || ""}`} {...other}>
