6e5109404c635f3413a1ae1a5a7b49a0ed654ec0
MicroBlock <microblock@qq.com>
2024-06-08 17:46:05 +0800

    fix: closing button

--- src/plugin-manager/components/button.tsx
+++ src/plugin-manager/components/button.tsx
@@ -1,9 +1,13 @@
+import { useMemo } from "react";
+
 export const Button: React.FC<
 	React.PropsWithChildren<React.HTMLAttributes<HTMLAnchorElement>>
 > = (props) => {
 	const { children, className, ...other } = props;
-	const is300 = APP_CONF.appver.startsWith('3.')
-	const wrapperClass = is300 && [...((document.querySelector('.cmd-button'))!.classList as unknown as string[])].find(v => v.startsWith('ButtonWrapper_'))
+	const wrapperClass = useMemo(() => {
+		const is300 = APP_CONF.appver.startsWith('3.')
+		return is300 && [...((document.querySelector('.cmd-button'))!.classList as unknown as string[])].find(v => v.startsWith('ButtonWrapper_'))
+	}, [])
 	return (
 		<a className={`${wrapperClass} u-ibtn5 u-ibtnsz8 cmd-button cmd-button-outlineSec cmd-button-size-m cmd-button-outline-sec button-item
 		 ${className || ""}`} {...other}>
